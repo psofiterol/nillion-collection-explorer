ee77661d38a15cb80542f0ebe23edd3019af96b1
oceans404 <steph.orpilla@nillion.com>
2025-08-25 11:30:47 -0700

    add BoN favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,11 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="stylesheet" href="/nillion.css" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <script
           defer
           src="https://cloud.umami.is/script.js"
